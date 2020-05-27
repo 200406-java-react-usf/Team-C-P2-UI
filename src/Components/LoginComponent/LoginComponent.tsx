15189a5537efcc2bd18d653ab10f6578dd6ed8c7
khkeipe <kkeipe@gmail.com>
2020-05-27 08:52:41 -0500

    basic entry field and link added to login page

--- src/Components/LoginComponent/LoginComponent.tsx
+++ src/Components/LoginComponent/LoginComponent.tsx
@@ -2,23 +2,38 @@ import React from 'react';
 import { User } from '../../models/user';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { TextField } from '@material-ui/core';
-import classes from '*.module.css';
+import { Link } from 'react-router-dom';
 
 interface ILoginProps {
-	authUser: User;
-	errorMessage: String;
-	loginAction: (username: string, password: string) => void;
+	// authUser: User;
+	// errorMessage: String;
+	// loginAction: (username: string, password: string) => void;
 }
 
+const useStyles = makeStyles((theme: Theme) => 
+	createStyles({
+	root: {
+		'& > *': {
+		  margin: theme.spacing(1),
+		  width: '25ch',
+		}},
+	link: {
+		
+	}
+}));
 
-function LoginComponent() {
 
+function LoginComponent(props: ILoginProps) {
+
+	const classes = useStyles();
 
 	return (
 		<>
 			<form className={classes.root} noValidate autoComplete="off" >
-				<TextField id="outlined-basic" label="Outlined" variant="outlined" />
+				<TextField id="outlined-basic" label="Username" variant="outlined" />
+				<TextField id="outlined-basic" label="Password" type="password" variant="outlined" />
 			</form>
+			<Link to="" className={classes.link}> LOGIN </Link>
 		</>
 	)
 }
