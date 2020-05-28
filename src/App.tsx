e253180e1b7ca973557686492d71e43db652a9c1
khkeipe <kkeipe@gmail.com>
2020-05-27 21:38:39 -0500

    wrap provider around jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'; 
-import LoginComponent from './Components/LoginComponent/LoginComponent';
+import LoginComponent from './Components/LoginComponent/LoginContainer';
 import { store } from './Store';
 import { Provider } from 'react-redux';
 
 function App() {
   return (
     <>
+    <Provider store={store}>
       <Router>
 
 
         <Switch>
-          <Route path="/login" render={() => <LoginComponent/> } />
+          <Route path="/login" render={() => <LoginComponent /> } />
         </Switch>
       </Router>
+    </Provider> 
     </>
   );
 }
