30bafc2673850597b1bbed041aa46c7d69648fe5
Korey <kkeipe@gmail.com>
2020-05-27 19:33:07 -0500

    basic login component layout complete

--- src/Components/LoginComponent/LoginComponent.tsx
+++ src/Components/LoginComponent/LoginComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { User } from '../../models/user';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import { TextField } from '@material-ui/core';
+import { TextField, Grid, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 interface ILoginProps {
@@ -12,13 +12,29 @@ interface ILoginProps {
 
 const useStyles = makeStyles((theme: Theme) => 
 	createStyles({
+	
+	loginContainer: {
+		display: "flex",
+        justifyContent: "center",
+        margin: 20,
+        marginTop: 40,
+        padding: 20
+	},
 	root: {
 		'& > *': {
 		  margin: theme.spacing(1),
-		  width: '25ch',
-		}},
+		  maxwidth: '25ch',
+		},
+		textAlign: 'center',
+		maxWidth: '50%'
+
+	}, 
+	form: {
+		justifyContent: 'center',
+	},
 	link: {
-		
+		textDecoration: 'none',
+		fontWeight: 'bolder'
 	}
 }));
 
@@ -27,13 +43,30 @@ function LoginComponent(props: ILoginProps) {
 
 	const classes = useStyles();
 
+	const login = async () => {
+
+		console.log("Login Button Clicked");
+
+	}
+
 	return (
 		<>
-			<form className={classes.root} noValidate autoComplete="off" >
-				<TextField id="outlined-basic" label="Username" variant="outlined" />
-				<TextField id="outlined-basic" label="Password" type="password" variant="outlined" />
-			</form>
-			<Link to="" className={classes.link}> LOGIN </Link>
+		<div className={classes.loginContainer}>
+				<br/>
+			<Grid className={classes.root}>
+				<form className={classes.form} noValidate autoComplete="off" >
+					<TextField id="outlined-basic" label="Username" variant="outlined" />
+						<br/>
+					<TextField id="outlined-basic" label="Password" type="password" variant="outlined" />
+				</form>
+
+				<Link to="/login" className={classes.link}> 
+					<Button onClick={login} variant="contained">LOGIN</Button>
+				</Link>
+
+			</Grid>
+
+		</div>	
 		</>
 	)
 }
