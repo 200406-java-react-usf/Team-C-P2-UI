dc4be55e42c28e03410c46284fcba2a0768d0eda
khkeipe <kkeipe@gmail.com>
2020-05-28 09:07:10 -0500

    adding nav bar to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'; 
 import LoginComponent from './Components/LoginComponent/LoginContainer';
+import NavBarComponent from './Components/NavBarComponent/NavBarComponent';
 import { store } from './Store';
 import { Provider } from 'react-redux';
 
@@ -10,7 +11,7 @@ function App() {
     <>
     <Provider store={store}>
       <Router>
-
+        <NavBarComponent />
 
         <Switch>
           <Route path="/login" render={() => <LoginComponent /> } />
