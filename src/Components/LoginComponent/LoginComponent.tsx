bef662a1955bea1033ee8c44049f3dc249b039ff
khkeipe <kkeipe@gmail.com>
2020-05-27 21:45:33 -0500

    adding methods to button

--- src/Components/LoginComponent/LoginComponent.tsx
+++ src/Components/LoginComponent/LoginComponent.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { User } from '../../models/user';
+import React, { useState } from 'react';
+import { User } from '../../dtos/user';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { TextField, Grid, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Alert } from '@material-ui/lab';
+import { Link, Redirect } from 'react-router-dom';
 
 interface ILoginProps {
-	// authUser: User;
-	// errorMessage: String;
-	// loginAction: (username: string, password: string) => void;
+	authUser: User;
+	errorMessage: String;
+	loginAction: (username: string, password: string) => void;
 }
 
 const useStyles = makeStyles((theme: Theme) => 
@@ -43,27 +44,45 @@ function LoginComponent(props: ILoginProps) {
 
 	const classes = useStyles();
 
-	const login = async () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
+	let updateLoginForm = (e: any) => {
+		switch(e.target.id) {
+			case 'username':
+				setUsername(e.target.value);
+				break;
+			case 'password':
+				setPassword(e.target.value);
+				break;
+			default:
+				console.warn(`Improper binding detected on element with id: ${e.target.id}`);
+		}
+	}
+
+	const login = async () => {
+		props.loginAction(username, password);
 		console.log("Login Button Clicked");
 
 	}
 
 	return (
+		props.authUser ? <Redirect to="" /> :
 		<>
 		<div className={classes.loginContainer}>
 				<br/>
 			<Grid className={classes.root}>
 				<form className={classes.form} noValidate autoComplete="off" >
-					<TextField id="outlined-basic" label="Username" variant="outlined" />
+					<TextField onChange={updateLoginForm} id="outlined-basic" label="Username" variant="outlined" />
 						<br/>
-					<TextField id="outlined-basic" label="Password" type="password" variant="outlined" />
+					<TextField onChange={updateLoginForm} id="outlined-basic" label="Password" type="password" variant="outlined" />
 				</form>
 
 				<Link to="/login" className={classes.link}> 
 					<Button onClick={login} variant="contained">LOGIN</Button>
 				</Link>
 
+				{	props.errorMessage ? <Alert severity="error" variant="outlined">{props.errorMessage}</Alert> : <></> }
 			</Grid>
 
 		</div>	
