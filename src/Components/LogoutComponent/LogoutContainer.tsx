eaa2206f0e85d3626dde7551099c04bbab5ce986
khkeipe <kkeipe@gmail.com>
2020-05-30 14:04:54 -0500

    adding logout container

---
+++ src/Components/LogoutComponent/LogoutContainer.tsx
@@ -0,0 +1,17 @@
+import { IState } from '../../reducers';
+import { connect } from 'react-redux';
+import { logoutAction } from '../../actions/logout-action';
+import LogoutComponent from '../LogoutComponent/LogoutComponent';
+
+const mapStateToProps = (state: IState) => {
+	return {
+		authUser: state.logout.authUser,
+		errorMessage: state.logout.errorMessage
+	}
+}
+
+const mapDispatchToProps = {
+	logoutAction
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogoutComponent);
