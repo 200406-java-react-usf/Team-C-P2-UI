2fc75172dc0d9a3d78776b578af36b9092664e01
khkeipe <kkeipe@gmail.com>
2020-05-30 13:20:07 -0500

    adding save and getall user axios requests

--- src/remote/user-service.ts
+++ src/remote/user-service.ts
@@ -1,2 +1,18 @@
+import { NewUser } from '../dtos/newUser'
+import { User } from '../dtos/user';
+import { travelClient } from './travel-client';
 
-export {}
+
+export async function getAllUsers(): Promise<User[]> {
+
+	let response = await travelClient.get("/user");
+	console.log(response.data);
+	return await response.data;
+}
+
+export async function save(newUser: NewUser): Promise<User> {
+
+	let response = await travelClient.post("/users", newUser);
+	console.log(response.data);
+	return await response.data;
+}
