34d2614c357573e438751966d062716da1d7c94d
khkeipe <kkeipe@gmail.com>
2020-05-31 14:45:36 -0500

    adding route for UserInfoComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'; 
 import LoginComponent from './Components/LoginComponent/LoginContainer';
 import NavBarComponent from './Components/NavBarComponent/NavBarContainer';
 import { store } from './Store';
@@ -11,17 +11,16 @@ import LogoutComponent from './Components/LogoutComponent/LogoutContainer';
 import RegisterComponent from './Components/RegisterComponent/RegisterContainer';
 import CreateTicketComponent from './Components/CreateTicketComponent/CreateTicketComponent';
 
-
 function App() {
   return (
     <>
-      <h1>Hello</h1>
-      <UserInfoComponent />
     <Provider store={store}>
       <Router>
         <NavBarComponent />
 
         <Switch>
+          <Redirect  from="/" to="/login" exact />
+          
           <Route path="/login" render={() => <LoginComponent /> } />
 
           <Route path="/logout" render={() => <LogoutComponent /> } />
@@ -29,6 +28,8 @@ function App() {
           <Route path="/register" render={() => <RegisterComponent /> } />
 
           <Route path="/create" render={() => <CreateTicketComponent/> } />
+
+          <Route path="/users" render={() => <UserInfoComponent /> } />
 
         </Switch>
       </Router>
