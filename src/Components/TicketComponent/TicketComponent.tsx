e6c36cd772b8188e58619af9a8d091b98076d176
khkeipe <kkeipe@gmail.com>
2020-05-31 12:55:31 -0500

    adding ticket component

---
+++ src/Components/TicketComponent/TicketComponent.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect } from 'react';
+import { makeStyles, Card } from '@material-ui/core';
+
+import MaterialTable, { Column } from 'material-table';
+
+interface Row {
+  name: string;
+  surname: string;
+  birthYear: number;
+  birthCity: number;
+}
+
+interface TableState {
+  columns: Array<Column<Row>>;
+  data: Row[];
+}
+
+const useStyles = makeStyles({
+	Container: {
+		display: "flex",
+        justifyContent: "center",
+        margin: 'auto',
+        marginTop: 40,
+		padding: 20,
+		maxWidth: '85%',
+		backgroundColor: '#48967D'
+	},
+	media: {
+	  height: 140,
+	},
+  });
+
+function TicketComponent() {
+
+	const classes = useStyles();
+
+	const [state, setState] = React.useState<TableState>({
+		columns: [
+		  { title: 'Name', field: 'name' },
+		  { title: 'Surname', field: 'surname' },
+		  { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+		  {
+			title: 'Birth Place',
+			field: 'birthCity',
+			lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+		  },
+		],
+		data: [
+		  { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+		  {
+			name: 'Zerya Betül',
+			surname: 'Baran',
+			birthYear: 2017,
+			birthCity: 34,
+		  },
+		],
+	  });
+	return (
+		<> 
+		<Card className={classes.Container}>
+		 <MaterialTable
+      		title="Editable Example"
+			columns={state.columns}
+			data={state.data}
+			editable={{
+				onRowAdd: (newData) =>
+				new Promise((resolve) => {
+					setTimeout(() => {
+					resolve();
+					setState((prevState) => {
+						const data = [...prevState.data];
+						data.push(newData);
+						return { ...prevState, data };
+					});
+					}, 600);
+				}),
+				onRowUpdate: (newData, oldData) =>
+				new Promise((resolve) => {
+					setTimeout(() => {
+					resolve();
+					if (oldData) {
+						setState((prevState) => {
+						const data = [...prevState.data];
+						data[data.indexOf(oldData)] = newData;
+						return { ...prevState, data };
+						});
+					}
+					}, 600);
+				}),
+				onRowDelete: (oldData) =>
+				new Promise((resolve) => {
+					setTimeout(() => {
+					resolve();
+					setState((prevState) => {
+						const data = [...prevState.data];
+						data.splice(data.indexOf(oldData), 1);
+						return { ...prevState, data };
+					});
+					}, 600);
+				}),
+			}}
+			/>
+			</Card>
+		</>
+	)
+}
+
+export default TicketComponent;
