d86df50797136bd095886f825eb0bd848e643bac
khkeipe <kkeipe@gmail.com>
2020-06-02 23:09:55 -0500

    adding delete ticket request

--- src/remote/ticket-service.ts
+++ src/remote/ticket-service.ts
@@ -5,3 +5,9 @@ export async function getTickets() {
 	let response = await travelClient.get('/tickets');
 	return await response.data;
 }
+
+export async function deleteTicketByID(id: number) {
+
+	let response = await travelClient.delete(`/tickets/${id}`)
+	return response.data;
+}
